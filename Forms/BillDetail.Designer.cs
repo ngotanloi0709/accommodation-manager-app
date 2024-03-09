ca4faaa21b3c4abd1c80d826141161f0d1ea7c12
Khánh Huân <www.khanhhuan@gmail.com>
2024-03-09 16:23:49 +0700

    SE2 Bill Export Feature

---
+++ Forms/BillDetail.Designer.cs
@@ -0,0 +1,329 @@
+﻿namespace AccommodationManagerApp.Forms
+{
+    partial class BillDetail
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PanelHD = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.LabelTittle = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.btn_ExportPDF = new System.Windows.Forms.Button();
+            this.PanelHD.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PanelHD
+            // 
+            this.PanelHD.Controls.Add(this.label17);
+            this.PanelHD.Controls.Add(this.label15);
+            this.PanelHD.Controls.Add(this.label16);
+            this.PanelHD.Controls.Add(this.label13);
+            this.PanelHD.Controls.Add(this.label14);
+            this.PanelHD.Controls.Add(this.label11);
+            this.PanelHD.Controls.Add(this.label12);
+            this.PanelHD.Controls.Add(this.label9);
+            this.PanelHD.Controls.Add(this.label10);
+            this.PanelHD.Controls.Add(this.label7);
+            this.PanelHD.Controls.Add(this.label8);
+            this.PanelHD.Controls.Add(this.label5);
+            this.PanelHD.Controls.Add(this.label6);
+            this.PanelHD.Controls.Add(this.label3);
+            this.PanelHD.Controls.Add(this.label4);
+            this.PanelHD.Controls.Add(this.label2);
+            this.PanelHD.Controls.Add(this.label1);
+            this.PanelHD.Controls.Add(this.LabelTittle);
+            this.PanelHD.Controls.Add(this.pictureBox1);
+            this.PanelHD.Dock = System.Windows.Forms.DockStyle.Top;
+            this.PanelHD.Location = new System.Drawing.Point(0, 0);
+            this.PanelHD.Name = "PanelHD";
+            this.PanelHD.Size = new System.Drawing.Size(600, 547);
+            this.PanelHD.TabIndex = 0;
+            this.PanelHD.Paint += new System.Windows.Forms.PaintEventHandler(this.panelHD_Paint);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(600, 81);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(290, 263);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(93, 25);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Tiền điện";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(123, 263);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Tiền điện:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(290, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 25);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Tiền phòng";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(123, 219);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(117, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Tiền phòng:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(292, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Mã hóa đơn";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(125, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 25);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Mã hóa đơn:";
+            // 
+            // LabelTittle
+            // 
+            this.LabelTittle.AutoSize = true;
+            this.LabelTittle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LabelTittle.Location = new System.Drawing.Point(123, 84);
+            this.LabelTittle.Name = "LabelTittle";
+            this.LabelTittle.Size = new System.Drawing.Size(361, 25);
+            this.LabelTittle.TabIndex = 8;
+            this.LabelTittle.Text = "HÓA ĐƠN THANH TOÁN NHÀ TRỌ";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(290, 304);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 25);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Tiền nước";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(123, 304);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 25);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Tiền nước:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(290, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 25);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Mã phòng";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(123, 174);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(106, 25);
+            this.label10.TabIndex = 17;
+            this.label10.Text = "Mã phòng:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(290, 345);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(73, 25);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Chi phí";
+            this.label11.Click += new System.EventHandler(this.label11_Click);
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(123, 345);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(163, 25);
+            this.label12.TabIndex = 19;
+            this.label12.Text = "Chi phí phát sinh:";
+            this.label12.Click += new System.EventHandler(this.label12_Click);
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(290, 389);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(94, 25);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Tổng tiền";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(123, 389);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(94, 25);
+            this.label14.TabIndex = 21;
+            this.label14.Text = "Tổng tiền";
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(290, 435);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(89, 25);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "Ngày lặp";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(123, 435);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(89, 25);
+            this.label16.TabIndex = 23;
+            this.label16.Text = "Ngày lặp";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(84, 512);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(400, 25);
+            this.label17.TabIndex = 25;
+            this.label17.Text = "Cảm ơn quý khách đã chọn nhà trọ chúng tôi";
+            this.label17.Click += new System.EventHandler(this.label17_Click);
+            // 
+            // btn_ExportPDF
+            // 
+            this.btn_ExportPDF.AutoSize = true;
+            this.btn_ExportPDF.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_ExportPDF.Location = new System.Drawing.Point(232, 571);
+            this.btn_ExportPDF.Name = "btn_ExportPDF";
+            this.btn_ExportPDF.Size = new System.Drawing.Size(100, 34);
+            this.btn_ExportPDF.TabIndex = 1;
+            this.btn_ExportPDF.Text = "Xuất PDF";
+            this.btn_ExportPDF.UseVisualStyleBackColor = true;
+            this.btn_ExportPDF.Click += new System.EventHandler(this.btn_ExportPDF_Click);
+            // 
+            // BillDetail
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 651);
+            this.Controls.Add(this.btn_ExportPDF);
+            this.Controls.Add(this.PanelHD);
+            this.Name = "BillDetail";
+            this.Text = "BillDetail";
+            this.PanelHD.ResumeLayout(false);
+            this.PanelHD.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel PanelHD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label LabelTittle;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Button btn_ExportPDF;
+    }
+}
---
+++ Forms/BillDetail.cs
@@ -0,0 +1,106 @@
+﻿using PdfSharp.Drawing;
+using PdfSharp.Pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AccommodationManagerApp.Forms
+{
+    public partial class BillDetail : Form
+    {
+        public BillDetail()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panelHD_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_ExportPDF_Click(object sender, EventArgs e)
+        {
+            // Tạo hộp thoại lưu file
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PDF Files|*.pdf";
+            saveFileDialog.Title = "Save as PDF";
+            saveFileDialog.FileName = "BillDetail.pdf";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+
+                try
+                {
+                    PdfDocument pdf = new PdfDocument();
+                    pdf.Info.Title = "Bill Detail";
+
+                    PdfPage pdfPage = pdf.AddPage();
+                    XGraphics graph = XGraphics.FromPdfPage(pdfPage);
+
+                    // Lấy nội dung của PanelHD và đưa nó vào tài liệu PDF
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        // Lấy nội dung của PanelHD
+                        Bitmap bmp = new Bitmap(PanelHD.Width, PanelHD.Height);
+                        PanelHD.DrawToBitmap(bmp, new System.Drawing.Rectangle(0, 0, PanelHD.Width, PanelHD.Height));
+
+                        // Chuyển đổi hình ảnh từ System.Drawing.Image sang dạng byte[]
+                        byte[] imageBytes;
+                        using (MemoryStream imageStream = new MemoryStream())
+                        {
+                            bmp.Save(imageStream, System.Drawing.Imaging.ImageFormat.Png);
+                            imageBytes = imageStream.ToArray();
+                        }
+
+                        XImage panelImage = XImage.FromStream(new MemoryStream(imageBytes));
+
+                        // Tính toán vị trí canh giữa trên trang PDF không quan tâm đến rotation
+                        double x = (pdfPage.Width - panelImage.PixelWidth) / 2;
+                        double y = (pdfPage.Height - panelImage.PixelHeight) / 2;
+
+                        // Vẽ hình ảnh vào tài liệu PDF với vị trí canh giữa
+                        graph.DrawImage(panelImage, x, y);
+                    }
+
+                    pdf.Save(filePath);
+
+                    // Thông báo khi hoàn thành xuất PDF
+                    MessageBox.Show("Xuất PDF thành công!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch (Exception ex)
+                {
+                    // Xử lý lỗi khi xuất PDF
+                    MessageBox.Show("Lỗi khi xuất PDF: " + ex.Message, "Lỗi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+    }
+}
